7b0f7d784b51e5262e808326e48e088b88284502
CodeCraft <86466022+CodeCraftPlugin@users.noreply.github.com>
2023-05-27 21:48:26 +0530

    Initial commit

---
+++ src/main/java/me/codecraft/darkendepths/DarkenDepths.java
@@ -0,0 +1,2 @@
+package me.codecraft.darkendepths;public class DarkenDepths {
+}
---
+++ src/main/java/me/codecraft/darkendepths/client/DarkenDepthsClient.java
@@ -0,0 +1,10 @@
+package me.codecraft.darkendepths;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DarkenDepthsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        System.out.println("Hello Fabric world! (Client)");
+    }
+}
